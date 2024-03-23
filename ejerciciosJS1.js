e36b36ba13fae0c884c9d27955e5b247c94a5e06
DariRandone <cdr00002011@gmail.com>
2024-03-23 12:00:09 -0300

    fin por hoy

--- ejerciciosJS1.js
+++ ejerciciosJS1.js
@@ -290,14 +290,17 @@ return area
 
 }
 console.log(areaDelTriangulo(4,5))
-/*
+
 function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. 
   //Escribe un programa que pida al usuario un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-
+let euros=prompt(" Introduzca la catidad de Euros a convertir","")
+let dolares = euros / 1.2
+return "Son ",dolares," dólares"
 }
-
+console.log("Son ", deEuroAdolar , "dólares")
+/*
 
 function esVocal(letra){
   //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
