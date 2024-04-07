cf3b4933dbd0bb66516db5a446548dad8dfd712e
DariRandone <cdr00002011@gmail.com>
2024-04-07 20:44:02 -0300

    No funca

--- Clase5/rickymorty.js
+++ Clase5/rickymorty.js
@@ -1,8 +1,23 @@
+const main_container =document.getElementById('characters')
 const getCharacters =async()=> {
-    const response =await fetch('https://rickandmortyapi.com/api/character');
+    const response = await fetch('https://rickandmortyapi.com/api/character');
     const data = await response.json();
 
-    console.log(data);
+   // console.log(data);
+    const {info,result} = data;
+    results.forEach(character => {
+        const article = document.createElement('article');
+        article.innerHTML =`
+            <img src = "${character.image}" alt=""> 
+            <h2> ${character.name} </h2>
+            <div>
+            <p>${character.species}</p>
+            <p>${character.status}</p>
+            </div>
+            <a href="">Ver personaje</a>
+            ` 
+            main_container.appendChild(article)     
+    });
 
 }
 getCharacters();
