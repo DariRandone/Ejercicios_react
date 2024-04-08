0e6383b8e33f8d5e870447d67761b41c7d23a4c5
DariRandone <cdr00002011@gmail.com>
2024-04-07 21:05:00 -0300

    Arreglado

--- Clase5/rickymorty.js
+++ Clase5/rickymorty.js
@@ -1,22 +1,22 @@
-const main_container =document.getElementById('characters')
+const main_container = document.getElementById('characters');
 const getCharacters =async()=> {
     const response = await fetch('https://rickandmortyapi.com/api/character');
     const data = await response.json();
 
    // console.log(data);
-    const {info,result} = data;
+    const {info, results} = data;
     results.forEach(character => {
         const article = document.createElement('article');
         article.innerHTML =`
-            <img src = "${character.image}" alt=""> 
+            <img src = "${character.image}" alt="Imagen del personaje ${character.name}"> 
             <h2> ${character.name} </h2>
             <div>
             <p>${character.species}</p>
             <p>${character.status}</p>
             </div>
             <a href="">Ver personaje</a>
-            ` 
-            main_container.appendChild(article)     
+            ` ;
+        main_container.appendChild(article);     
     });
 
 }
